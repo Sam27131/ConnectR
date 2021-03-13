5e944a8788466ba371e7f380c699491781d21553
Usama <usama_a123@hotmail.com>
2021-03-12 20:44:33 -0500

    Added images and some maps

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,27 @@ const connectr = {};
 // Organizing init function to pass to document ready
 connectr.init = () => {
   connectr.slideshow();
+  connectr.toggleTour(".imagesButton", ".imagesContainer");
+  connectr.toggleTour(".virtualButton", ".virtualContainer");
+  connectr.closeWindow(".closeImages", ".imagesContainer");
+  connectr.closeWindow(".closeVirtual", ".virtualContainer");
 };
 
 connectr.slideshow = () => {
   $(".main-carousel").flickity({
     groupCells: true,
+  });
+};
+
+connectr.toggleTour = (selector, container) => {
+  $(selector).click(function () {
+    $(container).toggle(500, "linear");
+  });
+};
+
+connectr.closeWindow = (selector, container) => {
+  $(selector).click(function () {
+    $(container).hide(500);
   });
 };
 
